db6e8ae17498f39b7a25386080fbc3bbdaa65c91
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2022-07-28 15:03:22 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,35 @@
 "use strict";
 
-let js = "amazing";
+// let js = "amazing";
 
-console.log(40 + 8 + 23 - 10);
-console.log("Jonas");
-console.log(23);
+// console.log(40 + 8 + 23 - 10);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Jonas";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// true;
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
+
+// console.log(typeof null);
