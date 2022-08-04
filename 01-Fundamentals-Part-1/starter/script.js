cc6207d8a80937fabd135a3b9b53282044bf686e
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2022-08-04 15:09:50 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -73,19 +73,83 @@
 
 // console.log(now - 1991 > now - 2018);
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
 
-// console.log(25 - 10 - 5);
+// // console.log(25 - 10 - 5);
 
-let x, y;
-x = y = 25 - 10 - 5; // x = y = 10;
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10;
 
-console.log(x, y);
+// console.log(x, y);
 
-const averageAge = (ageJonas + ageSarah) / 2;
+// const averageAge = (ageJonas + ageSarah) / 2;
 
-console.log(ageJonas, ageSarah, averageAge);
+// console.log(ageJonas, ageSarah, averageAge);
+
+// Coding Challenge #1
+
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both versions)
+// 3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+// TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+// TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+// Test DATA 1 //
+// Mark //
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const BMIMark = massMark / heightMark ** 2;
+
+// // John //
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIJohn = massJohn / heightJohn ** 2;
+// // compare //
+// console.log(BMIMark, BMIJohn);
+
+// const markHigherBMI = BMIMark > BMIJohn; // true or false //
+
+// console.log(markHigherBMI); // true //
+
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = "1991";
+// const year = 2037;
+
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(jonasNew);
+
+// console.log(`just a regular string...`);
+
+// console.log(`String
+// multiple
+// lines `);
+
+// const age = 19;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//   console.log("Sarah can start driving.");
+// }
+
+const age = 15;
+if (age >= 18) {
+  console.log("Sarah can start driving.");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(yearsLeft);
+}
