dddfadf898227c2d9683c4cd7ea23fda826c4905
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-07-02 18:44:37 -0500

    functions
    declaration and expression functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,8 +2,34 @@
 
 // Functions //
 
-function logger() {
-  console.log("My name is Greg.");
-}
+// function logger() {
+//   console.log("My name is Greg.");
+// }
 
-logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// Function Declaration
+
+function calcAge1(birthDate) {
+  return 2037 - birthDate;
+}
+const age1 = calcAge1(1981);
+
+// Function Expression
+
+const calcAge2 = function (birthDate) {
+  return 2037 - birthDate;
+};
+const age2 = calcAge2(1981);
+console.log(age1, age2);
