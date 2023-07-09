d104fec2949b17bf2f2a219bcf110e55a7e36888
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-07-09 14:49:33 -0500

    still working

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,12 +34,35 @@
 // const age2 = calcAge2(1981);
 // console.log(age1, age2);
 
-const calcAge2 = function (birthDate) {
-  return 2037 - birthDate;
-};
+// const calcAge2 = function (birthDate) {
+//   return 2037 - birthDate;
+// };
 
 // Arrow function
 
-const calcAge3 = (birthDate) => 2037 - birthDate;
-const age3 = calcAge3(1981);
-console.log(age3);
+// const calcAge3 = (birthDate) => 2037 - birthDate;
+// const age3 = calcAge3(1981);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthDate, firstName) => {
+//   const age = 2037 - birthDate;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years`;
+// };
+
+// console.log(yearsUntilRetirement(1991, "Greg"));
+// console.log(yearsUntilRetirement(1980, "Bob"));
+
+// Function call function //
+
+function cutFruit(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruit(apples);
+  const orangePieces = cutFruit(oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
