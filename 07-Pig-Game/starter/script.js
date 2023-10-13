461daf89dc40de0b3856e11598701161b9b8c8c7
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-10-13 00:18:40 -0500

    Still working on pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// Selecting element
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Start at zero and players
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Hide dice.
+diceEl.classList.add('hidden');
+
+// Roll dice function
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
