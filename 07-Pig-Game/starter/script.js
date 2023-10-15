ed70e4a2ff4bc6c8102503e94dcd0085561af75b
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-10-15 15:13:44 -0500

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,11 +1,17 @@
 'use strict';
 
 // Selecting element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
+
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -31,7 +37,18 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
-    // Switch player
+    // Switch player - Set score to Zero.
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
+});
+
+// Hold score - Add current score - check score is >= 100- finish game/switch player
+btnHold.addEventListener('click', () => {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    scores[activePlayer];
 });
