2e73dea6ec6e9e52295e27d27ca846cc1f54b911
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-10-16 21:05:28 -0500

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,14 +1,50 @@
 'use strict';
+// Scope
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//   function printAge() {
+//     const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
+//     console.log(output);
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
-    console.log(output);
-  }
-  printAge();
-  return age;
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const firstName = 'Steven';
+//       const str = `Oh, and your a millennial, ${firstName}.`;
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       console.log(str);
+//     }
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+// Hoisting and temporal dead zone (TDZ)
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
