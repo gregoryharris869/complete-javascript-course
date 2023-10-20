3889fd94f4637630c67f4b7540b327b1c1e5a43b
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-10-20 14:37:00 -0500

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -143,3 +143,62 @@
 // };
 
 // addArrow(2, 5, 8);
+
+// Primitives vs. Objects (Primitives vs. Reference Types)
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age, oldAge);
+
+// const me = {
+//   name: 'Jonas',
+//   age: 30,
+// };
+
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend', friend);
+// console.log('Me', me);
+
+// Primitives vs. Objects Practice
+
+// Primitive Types
+// let lastName = 'Williams';
+// let oldLastName = lastName;
+// lastName = 'Davis';
+// console.log(lastName, oldLastName);
+
+// // Reference TYpe
+// const jessica = {
+//   firstName: 'Jessica',
+//   lastName: 'Williams',
+//   age: 27,
+// };
+
+// const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+// console.log('Before marriage:', jessica);
+// console.log('After marriage:', marriedJessica);
+
+// // Copy objects
+
+// const jessica2 = {
+//   firstName: 'Jessica',
+//   lastName: 'Williams',
+//   age: 27,
+//   family: ['Alice', 'Bob'],
+// };
+
+// // Shallow copy - first level
+// const jessicaCopy = Object.assign({}, jessica2);
+// jessicaCopy.lastName = 'Davis';
+
+// console.log('Before marriage:', jessica2);
+// console.log('After marriage:', jessicaCopy);
+
+// jessicaCopy.family.push('Mary');
+// jessicaCopy.family.push('John');
+
+// console.log('Before marriage:', jessica2);
+// console.log('After marriage:', jessicaCopy);
